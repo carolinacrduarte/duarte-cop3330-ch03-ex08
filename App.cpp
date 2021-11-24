0f36684ad85eeda5159bd6b7f265f4214d598f02
Carolina Duarte <carolcrduarte@knights.ucf.edu>
2021-11-24 12:02:22 -0500

    Added problem description.

--- App.cpp
+++ App.cpp
@@ -3,6 +3,10 @@
  *  Copyright 2021 Carolina Duarte
  */
 
+/*
+Write a program to test an integer value to determine if it is odd or even. As always, make sure your output is clear and complete. In other words, don’t just output yes or no. Your output should stand alone, like The value 4 is an even number. Hint: See the remainder (modulo) operator in §3.4.
+*/
+
 //Header file.
 #include "std_lib_facilities.h"
 
